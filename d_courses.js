e7147faad75cbd49527a71d3e3113713e165b9d2
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2019-09-09 08:46:38 -0400

    Add files via upload

--- d_courses.js
+++ d_courses.js
@@ -2,5 +2,5 @@ function getData_Courses()
 {
 	arrCourseNames = new Array();
 	
-	return arrCourseNames = [	"Chapel Hill",	"Raymond Memorial",	"Homestead Springs",	"Champions",	"Phoenix",	"Raccoon",	"Upper Lansdowne",	"Thorn Apple",	"St. Albans",	"Cumberland Trail",	"Links at Echo Springs",	"Split Rock",	"Rolling Meadows",	"Kyber Run",	"Turnberry",	"Links at Groveport",	"Oakhaven",	"Blues Creek",	"Willow Run",	"Westchester",	"Foxfire",	"Royal American",	"Blacklick Woods",	"Timberview",	"Denison",	"National Golf Links",	"Darby Creek",	"The Ridge",	"Crystal Springs",	"Clover Valley",	"Blackhawk",	]
+	return arrCourseNames = [	"Blackhawk",	"Blacklick Woods",	"Blues Creek",	"Champions",	"Chapel Hill",	"Clover Valley",	"Crystal Springs",	"Cumberland Trail",	"Darby Creek",	"Denison",	"Foxfire",	"Homestead Springs",	"Kyber Run",	"Links at Echo Springs",	"Links at Groveport",	"National Golf Links",	"Oakhaven",	"Phoenix",	"Raccoon",	"Raymond Memorial",	"Rolling Meadows",	"Royal American",	"Split Rock",	"St. Albans",	"The Ridge",	"Thorn Apple",	"Timberview",	"Turnberry",	"Upper Lansdowne",	"Westchester",	"Willow Run"	]
 }
