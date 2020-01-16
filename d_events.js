05d0c9334b5538f49b070b4b05b5e5bb5218ae32
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2020-01-16 16:49:44 -0500

    Delete d_events.js

--- d_events.js
+++
@@ -1,5 +0,0 @@
-function getData_Events()
-{
-	arrEventNames = new Array();
-	return arrEventNames = [	"Rebel Beach Am-Am",	"Bastards",	"FORE Championship",	"F.U. Open",	"Bratish Open",	"MGA Championship",	"Douche Bag Invitational",	"Last Gasp"	]
-}
