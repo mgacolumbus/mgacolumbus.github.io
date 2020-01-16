65077620e730dbb28c11416c16f373f634b50174
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2020-01-16 16:43:30 -0500

    Delete f_getCurrentSeason.js

--- new/js/f_getCurrentSeason.js
+++
@@ -1,8 +0,0 @@
-function getCurrentSeason() {
-	/**---------------------------------------------------------------------**/
-		var arrSeasons	=	new Array();
-			arrSeasons	=	getData_Seasons();
-	/**---------------------------------------------------------------------**/
-	
-	return arrSeasons[arrSeasons.length - 1];
-}
