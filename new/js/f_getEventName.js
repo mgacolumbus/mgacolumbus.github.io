5c0b68ebea71f1a2b812162fe6b3b3d89a404752
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2020-01-16 16:43:58 -0500

    Delete f_getEventName.js

--- new/js/f_getEventName.js
+++
@@ -1,9 +0,0 @@
-function getEventName(EventIndex) {
-	/**---------------------------------------------------------------------**/
-    	var pEvent		=	arguments[0];
-		var arrEvents	=	new Array();
-			arrEvents	=	getData_Events();
-	/**---------------------------------------------------------------------**/
-	
-	return arrEvents[pEvent];
-}
