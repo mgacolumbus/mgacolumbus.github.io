8cb2a32845f5d7bdd0546277b3fe7105e44bb1b1
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2020-01-16 16:47:07 -0500

    Delete i_footer.js

--- new/js/i_footer.js
+++
@@ -1,7 +0,0 @@
-function html_Footer() {
-	document.write('<footer class="w3-container w3-padding-16 w3-light-grey">');
-	document.write('	<p>');
-	document.write('		Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>');
-	document.write('	</p>');
-	document.write('</footer>');
-}
