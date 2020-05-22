507964c48b5582973fa91fa44e73f3c435e8be40
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2020-05-22 14:49:56 -0400

    Add files via upload

---
+++ js/i_coursedetailtopboxes.js
@@ -0,0 +1,44 @@
+function html_CourseDetailTopBoxes(CourseData) {
+	/**---------------------------------------------------------------------**/
+		var pCourseRawData		=	arguments[0];
+	/**---------------------------------------------------------------------**/
+	
+	document.write('<div class="w3-row-padding w3-margin-bottom">');
+	document.write('	<div class="w3-quarter">');
+	document.write('		<div class="w3-container w3-green w3-padding-16">');
+	document.write('			<div class="w3-right">');
+	document.write('				<h3>' + '420.69' + '</h3>');
+	document.write('			</div>');
+	document.write('			<div class="w3-clear"></div>');
+	document.write('			<h4>MGA Average</h4>');
+	document.write('		</div>');
+	document.write('	</div>');
+	document.write('	<div class="w3-quarter">');
+	document.write('		<div class="w3-container w3-green w3-padding-16">');
+	document.write('			<div class="w3-right">');
+	document.write('				<h3>' + '6969' + ')</h3>');
+	document.write('			</div>');
+	document.write('			<div class="w3-clear"></div>');
+	document.write('			<h4>Total Rounds</h4>');
+	document.write('		</div>');
+	document.write('	</div>');
+	document.write('	<div class="w3-quarter">');
+	document.write('		<div class="w3-container w3-green w3-padding-16">');
+	document.write('			<div class="w3-right">');
+	document.write('				<h3>' + '69' + '</h3>');
+	document.write('			</div>');
+	document.write('			<div class="w3-clear"></div>');
+	document.write('			<h4>Low Round (gross)</h4>');
+	document.write('		</div>');
+	document.write('	</div>');
+	document.write('	<div class="w3-quarter">');
+	document.write('		<div class="w3-container w3-green w3-padding-16">');
+	document.write('			<div class="w3-right">');
+	document.write('				<h3>' + '420' + '</h3>');
+	document.write('			</div>');
+	document.write('			<div class="w3-clear"></div>');
+	document.write('			<h4>High Round</h4>');
+	document.write('		</div>');
+	document.write('	</div>');
+	document.write('</div>');
+}
