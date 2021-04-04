10aed019063843b3769aa5f8b83b1d51002a9c7d
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2021-04-04 09:02:52 -0400

    Add files via upload

--- js/i_eventwinners.js
+++ js/i_eventwinners.js
@@ -17,7 +17,7 @@ function html_EventWinnersList(Event) {
 		document.write('	<td style="vertical-align: middle;"><i class="fa fa-trophy w3-text-yellow w3-large"></i></td>');
 		document.write('	<td class="w3-center" style="vertical-align: middle;">' + varCurrentSeason + '</td>');
 		document.write('	<td class="w3-center" style="vertical-align: middle;">' + arrResults[0] + '</td>');
-		document.write('	<td class="w3-center" style="vertical-align: middle;">' + (arrResults[1][2] + arrResults[2]) + '</td>');
+		document.write('	<td class="w3-center" style="vertical-align: middle;">' + arrResults[1][2] + ' / ' + (arrResults[1][2] + arrResults[2]) + '</td>');
 		document.write('	<td><a class="w3-button w3-green w3-right" href="resultsdetails.html?s=' + varCurrentSeason + '&e=' + pEvent + '">+</a></td>');
 		document.write('<tr>');
 		varCurrentSeason -= 1;
