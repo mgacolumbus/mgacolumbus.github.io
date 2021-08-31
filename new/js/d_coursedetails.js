ea8672529867b3737dd3c4d1545d9a042d968931
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2021-08-31 09:32:55 -0400

    Add files via upload

---
+++ new/js/d_coursedetails.js
@@ -0,0 +1,50 @@
+function getData_CourseDetails() {
+	/**---------------------------------------------------------------------**/
+		var arrCourseDetails	=	new Array();
+	/**---------------------------------------------------------------------**/
+	
+	/*
+		arrCourseDetails[0]		=	Course Name
+		arrCourseDetails[1]		=	Tee Color
+		arrCourseDetails[2]		=	Yardage - Hole 1
+		arrCourseDetails[3]		=	Yardage - Hole 2
+		arrCourseDetails[4]		=	Yardage - Hole 3
+		arrCourseDetails[5]		=	Yardage - Hole 4
+		arrCourseDetails[6]		=	Yardage - Hole 5
+		arrCourseDetails[7]		=	Yardage - Hole 6
+		arrCourseDetails[8]		=	Yardage - Hole 7
+		arrCourseDetails[9]		=	Yardage - Hole 8
+		arrCourseDetails[10]	=	Yardage - Hole 9
+		arrCourseDetails[11]	=	Yardage - Hole 10
+		arrCourseDetails[12]	=	Yardage - Hole 11
+		arrCourseDetails[13]	=	Yardage - Hole 12
+		arrCourseDetails[14]	=	Yardage - Hole 13
+		arrCourseDetails[15]	=	Yardage - Hole 14
+		arrCourseDetails[16]	=	Yardage - Hole 15
+		arrCourseDetails[17]	=	Yardage - Hole 16
+		arrCourseDetails[18]	=	Yardage - Hole 17
+		arrCourseDetails[19]	=	Yardage - Hole 18
+	*/
+	
+	return arrCourseDetails = [
+
+[	"Blackhawk",	"Gold",	263,	295,	350,	152,	328,	140,	401,	310,	250,	257,	142,	414,	132,	300,	310,	363,	309,	297]
+,[	"Blackhawk",	"White",	336,	306,	431,	153,	380,	174,	482,	381,	328,	350,	144,	453,	152,	377,	349,	440,	404,	373]
+,[	"Blackhawk",	"Blue",	353,	356,	454,	188,	402,	209,	514,	404,	364,	371,	162,	485,	185,	402,	380,	459,	432,	404]
+
+,[	"Chapel Hill",	"Red",	306,	356,	150,	271,	212,	412,	290,	152,	309,	351,	287,	105,	194,	424,	211,	100,	226,	265]
+,[	"Chapel Hill",	"Gold",	310,	401,	150,	345,	310,	465,	295,	152,	310,	355,	290,	139,	282,	454,	280,	128,	290,	265]
+,[	"Chapel Hill",	"White",	366,	474,	160,	350,	315,	521,	390,	193,	412,	457,	376,	139,	285,	515,	281,	128,	292,	380]
+,[	"Chapel Hill",	"Blue",	398,	484,	165,	360,	336,	545,	411,	212,	420,	471,	399,	149,	315,	545,	300,	134,	310,	397]
+,[	"Chapel Hill",	"Black",	425,	505,	173,	400,	382,	560,	440,	245,	437,	494,	441,	155,	332,	581,	354,	153,	350,	429]
+
+,[	"Cumberland Trail",	"Red",	293,	433,	76,	298,	444,	79,	316,	316,	273,	301,	95,	381,	308,	313,	213,	340,	103,	302]
+,[	"Cumberland Trail",	"Gold",	317,	458,	102,	326,	472,	95,	345,	371,	308,	312,	105,	418,	339,	353,	257,	376,	150,	335]
+,[	"Cumberland Trail",	"White",	347,	493,	134,	365,	520,	163,	378,	404,	356,	334,	165,	506,	376,	400,	281,	443,	195,	367]
+,[	"Cumberland Trail",	"Blue",	384,	533,	160,	398,	536,	190,	405,	431,	401,	365,	197,	558,	397,	410,	319,	502,	213,	402]
+,[	"Cumberland Trail",	"Black",	405,	553,	180,	426,	567,	200,	430,	455,	421,	388,	210,	587,	418,	442,	351,	536,	227,	441]
+
+,[	"Phoenix",	"White",	523,	167,	315,	361,	405,	171,	287,	484,	375,	355,	511,	436,	418,	501,	135,	395,	151,	385]
+
+	]
+}
