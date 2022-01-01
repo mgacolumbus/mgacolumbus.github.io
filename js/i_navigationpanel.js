69bcd9cc1f7d8d1c24b6b14beb30820e0058150a
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2022-01-01 13:52:07 -0500

    .

--- js/i_navigationpanel.js
+++ js/i_navigationpanel.js
@@ -45,7 +45,7 @@ function html_NavigationPanel2() {
 
 	/**---------------------------------------------------------------------**/
 	
-	document.write('<nav class="navbar navbar-inverse">');
+	document.write('<nav class="navbar navbar-inverse navbar-fixed-top">');
 	document.write('	<div class="container-fluid">');
 	document.write('		<div class="navbar-header">');
 	document.write('			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">');
@@ -53,38 +53,54 @@ function html_NavigationPanel2() {
 	document.write('				<span class="icon-bar"></span>');
 	document.write('				<span class="icon-bar"></span>');
 	document.write('			</button>');
-	document.write('			<!--<a class="navbar-brand" href="http://mgatour.com/chapters/columbus" target="_chapter">MGA Columbus</a>-->');
+	document.write('			<a class="navbar-brand" href="index.html">Home</a>');
 	document.write('		</div>');
 	document.write('		<div class="collapse navbar-collapse" id="myNavbar">');
 	document.write('			<ul class="nav navbar-nav">');
-	document.write('				<li><a href="index.html">Home</a></li>');
 	document.write('				<li class="dropdown">');
 	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Stats <span class="caret"></span></a>');
 	document.write('					<ul class="dropdown-menu navbar-nav" style="background-color: #292929;">');
-	document.write('						<li><a href="moneylist.html?sb=' + getCurrentSeason() + '">Earnings List</a></li>');
-	document.write('						<li><a href="averagelist.html?sb=' + getCurrentSeason() + '">Average List</a></li>');
+	document.write('						<li><a href="#">Current Season Stats</a></li>');
+	document.write('						<li><a href="golferdetails.html">Golfer Stats</a></li>');
+	document.write('						<li><a href="#">Course Stats</a></li>');
+	document.write('						<!--<li><a href="#">Event Stats</a></li>-->');
+	document.write('						<li><a href="#">Chapter Stats</a></li>');
+	document.write('					</ul>');
+	document.write('				</li>');
+	document.write('				<li><a href="#">Event Results</a></li>');
+	document.write('				<li class="dropdown">');
+	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Records <span class="caret"></span></a>');
+	document.write('					<ul class="dropdown-menu navbar-nav" style="background-color: #292929;">');
+	document.write('						<li><a href="#">Scoring Records</a></li>');
+	document.write('						<li><a href="#">Placement Records</a></li>');
+	document.write('						<li><a href="#">Awards Records</a></li>');
+	document.write('						<li><a href="#">Chapter Records</a></li>');
 	document.write('					</ul>');
 	document.write('				</li>');
 	document.write('				<li class="dropdown">');
 	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Lists <span class="caret"></span></a>');
 	document.write('					<ul class="dropdown-menu navbar-nav" style="background-color: #292929;">');
+	document.write('						<li><a href="#">Event Scorecards</a></li>');
 	document.write('						<li><a href="eventchampions.html">Event Champions</a></li>');
 	document.write('					</ul>');
 	document.write('				</li>');
-	/*
-	document.write('				<li><a href="#">Results</a></li>');
-	document.write('				<li><a href="#">Records</a></li>');
-	*/
 	document.write('				<li class="dropdown">');
-	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Social <span class="caret"></span></a>');
+	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Social Media <span class="caret"></span></a>');
 	document.write('					<ul class="dropdown-menu navbar-nav" style="background-color: #292929;">');
-	document.write('						<li><a href="https://www.facebook.com/ColumbusMga" target="_fb"><span class="fa fa-facebook-square"></span> Facebook</a></li>');
-	document.write('						<li><a href="https://twitter.com/mgacolumbus" target="_tw"><span class="fa fa-twitter-square"></span> Twitter</a></li>');
-	document.write('						<li><a href="https://www.instagram.com/mgacolumbus/" target="_ig"><span class="fa fa-instagram"></span> Instagram</a></li>');
-	document.write('						<li><a href="https://discord.gg/ygQmfyv" target="_ds"><img src="images/discord2.png" width="19px" height="19px" /> Discord</a></li>');
+	document.write('						<li><a href="https://www.facebook.com/ColumbusMga" target="_fb"><span class="fa fa-facebook-square"></span>&nbsp;&nbsp;Facebook</a></li>');
+	document.write('						<li><a href="https://twitter.com/mgacolumbus" target="_tw"><span class="fa fa-twitter-square"></span>&nbsp;&nbsp;Twitter</a></li>');
+	document.write('						<li><a href="https://www.instagram.com/mgacolumbus/" target="_ig"><span class="fa fa-instagram"></span>&nbsp;&nbsp;Instagram</a></li>');
+	document.write('						<li><a href="https://discord.gg/ygQmfyv" target="_ds"><img src="images/discord2.png" width="19px" height="19px" />&nbsp;&nbsp;Discord</a></li>');
 	document.write('					</ul>');
 	document.write('				</li>');
-	document.write('				<li><a href="https://mgatour.com/chapters/contact-leader/1154" target="_em"><span class="fa fa-envelope"></span> Email Us</a></li>');
+	document.write('				<li class="dropdown">');
+	document.write('					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Misc. Stuff <span class="caret"></span></a>');
+	document.write('					<ul class="dropdown-menu navbar-nav" style="background-color: #292929;">');
+	document.write('						<li><a href="https://mgatour.com/chapters/contact-leader/1154" target="_em"><span class="fa fa-envelope"></span>&nbsp;&nbsp;Contact Us</a></li>');
+	document.write('						<li><a href="#">Chapter Rules</a></li>');
+	document.write('						<li><a href="#">Calendar</a></li>');
+	document.write('					</ul>');
+	document.write('				</li>');
 	document.write('			</ul>');
 	document.write('		</div>');
 	document.write('	</div>');
