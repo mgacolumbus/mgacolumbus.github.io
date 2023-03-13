2d20bc279f51b19be12c84d875080a8d9b61135f
mgacolumbus <44675371+mgacolumbus@users.noreply.github.com>
2023-03-13 17:26:34 -0400

    .

--- data/d_navigation.js
+++ data/d_navigation.js
@@ -7,28 +7,27 @@ function getData_Navigation() {
         arrNavigation[1]    =   Title
         arrNavigation[2]    =   URL
         arrNavigation[3]    =   Subtitle
-        arrNavigation[4]    =   Filter Settings
     */
     
     return arrNavigation = [
 	
 		// Main Menu Items
-        [0, "MGA Chapter Page", "http://mgatour.com/chapters/columbus", "", 0],
-        [0, "Records &amp; Rankings", "recordcategory.html", "", 0],
+        [0, "MGA Chapter Page", "http://mgatour.com/chapters/columbus", ""],
+        [0, "Records &amp; Rankings", "recordcategory.html", ""],
         
         [0, "Testing Page", "test.html", "", 0],
         
         // Recordlist Options
-        [1, "Career Scoring Records", "recordlist.html?n=2", "", 0],
-        [1, "Single Season Scoring Records", "recordlist.html?n=3", "", 0],
-        [1, "Single Event Scoring Records", "recordlist.html?n=4", "", 0],
-        [1, "Single Hole Scoring Records", "recordlist.html?n=5", "", 0],
+        [1, "Career Scoring Records", "recordlist.html?n=2", ""],
+        [1, "Single Season Scoring Records", "recordlist.html?n=3", ""],
+        [1, "Single Event Scoring Records", "recordlist.html?n=4", ""],
+        [1, "Single Hole Scoring Records", "recordlist.html?n=5", ""],
         
 		// Career Scoring Records
-		[2, "Career Wins", "record.html?r=1", "", 2310],
+		[2, "Career Wins", "record.html?r=1", ""],
         
 		// Single Season Scoring Records
-		[3, "Season Wins", "record.html?r=2", "", 30030],
+		[3, "Season Wins", "record.html?r=2", ""],
 		
     ]
     
--- data/d_recordlist.js
+++ data/d_recordlist.js
@@ -4,18 +4,15 @@ function getData_RecordList() {
 	/**---------------------------------------------------------------------**/
 	
 	/*
-		arrRecordList[0]	=	Record ID
-        arrRecordList[1]    =   Record Nav Title
         arrRecordList[2]    =   Record Title
         arrRecordList[3]    =   Record Subtitle
 		arrRecordList[4]	=	Record Filter Setting
-        arrRecordList[5]    =   Record Category
 	*/
 	
     arrRecordList = [
     
-        [0, "Career Wins", "Career Wins", "", 2310, 1],
-        [1, "Season Wins", "Single-Season Wins", "", 30030, 2]
+        ["Alt Career Wins", "", 2310],
+        ["Alt Single-Season Wins", "", 30030]
         
     ]
 
